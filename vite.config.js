24b8dda2889365efeb2a6a1d9364d756e6fe54e9
Björgvin Bessi Kristinsson <bjorgvin.kodehode@gmail.com>
2023-08-22 13:51:04 +0200

    fsdfg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/git-github.com-bjorgvinkodehode-reactportfolio",
+  base: "./git-github.com-bjorgvinkodehode-reactportfolio",
   plugins: [react()],
 })
