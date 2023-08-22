5060f821dfe94f66ec217ff7ab6b03e1cb99a48e
Björgvin Bessi Kristinsson <bjorgvin.kodehode@gmail.com>
2023-08-22 13:08:21 +0200

    changes to hashrouter from browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import './Header.css';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation  } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Projects from './Projects';
